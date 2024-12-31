52d08b905e7e042c00ad0b0dfe91bf16dbe075a6
Anshad UK <anshadu@gmail.com>
2024-12-31 21:07:31 +0530

    happy new year audio wish

---
+++ happy_new_year_audio_wish.py
@@ -0,0 +1,12 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say(
+    "Happy New Year 2025!"
+    "Wishing you a fantastic year ahead!"
+)
+
+engine.runAndWait()
+
+print("Happy New Year 2025! \
+      Wishing you a fantastic year ahead!")
+
